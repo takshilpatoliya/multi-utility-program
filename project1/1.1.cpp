66f2a99354c6f114a8807ab85f2bdf824285ffbf
takshilpatoliya <takshilpatoliya74@gmail.com>
2024-05-28 12:32:31 +0530

    Add files via upload

---
+++ project1/1.1.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+main()
+{
+	float f,c;
+	
+	printf(" enter the valueof c=");
+	scanf("%f",&c);
+	
+	f=(c*9/5)+32;
+	printf("f = %.2f ",f);
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
